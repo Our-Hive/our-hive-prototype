37c9d480aa74bf58a88ba49da28715326fe7ecce
snakydh <skdanielg5@gmail.com>
2023-06-04 23:36:55 -0500

    feat: add notifications

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import User from "../services/User";
+import { ToastContainer, toast } from "react-toastify";
 
 export default signup = () => {
   const [user, setUser] = useState({ username: "", password: "", email: "" });
@@ -10,9 +11,33 @@ export default signup = () => {
     userTemp.deleteUserNameLocalStorage();
     userTemp.saveDB();
     userTemp.saveUserNameLocalStorage(user.username);
+    toast("🐝 Cuenta Creada! 🍯", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: true,
+      theme: "light",
+      delay: 1,
+    });
   };
   return (
     <>
+      <ToastContainer
+        className="bg-amber-900 z-10 text-center font-bold fixed w-screen pt-4"
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Header />
       <main className="m-5">
         <h2 className="text-center text-xl font-bold">Register</h2>
